c35a021f631e8814880b43499d2612f214d8a4ff
Bevan Sinclair <bevan.sinclair@microsoft.com>
2024-05-23 16:14:27 +1200

    v3.7 updates

--- scripts/create-initiative.ps1
+++ scripts/create-initiative.ps1
@@ -5,14 +5,14 @@ $repoRoot = Join-Path $PSScriptRoot '..\'
 $jsonPath = Join-Path $repoRoot 'json'
 $csvPath = Join-Path $repoRoot 'csv'
 
-$initname = "nzism-3.6-policyset" 
-$initdisplayname = "New Zealand ISM Restricted v3.6" 
-$initdescription = "This initiative includes policies that address a subset of New Zealand Information Security Manual v3.6 controls. Additional policies will be added in upcoming releases. For more information, visit https://aka.ms/nzism-initiative." 
+$initname = "nzism-3.7-policyset" 
+$initdisplayname = "New Zealand ISM Restricted v3.7" 
+$initdescription = "This initiative includes policies that address a subset of New Zealand Information Security Manual v3.7 controls. Additional policies will be added in upcoming releases. For more information, visit https://aka.ms/nzism-initiative." 
 $initmetadata = "category=Regulatory Compliance","version=1.0"
-$initdefinitionsfile = Join-Path $jsonPath 'nzism3.6.definitions.json'
-$initparamsfile = Join-Path $jsonPath 'nzism3.6.parameters.json'
-$initgroupfile = Join-Path $jsonPath 'nzism3.6.groups.json'
-$outputFilePath = Join-Path $jsonPath 'nzism3.6.json'
+$initdefinitionsfile = Join-Path $jsonPath 'nzism3.7.definitions.json'
+$initparamsfile = Join-Path $jsonPath 'nzism3.7.parameters.json'
+$initgroupfile = Join-Path $jsonPath 'nzism3.7.groups.json'
+$outputFilePath = Join-Path $jsonPath 'nzism3.7.json'
 $parametersFilePath = Join-Path $csvPath 'params.csv'
 $policyFilePath = Join-Path $csvPath 'policies.csv'
 $controlsFilePath = Join-Path $csvPath 'controls.csv'
